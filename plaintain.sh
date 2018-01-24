4b30c729e1547fef5bd9dd7aa07164ab01bf8713
Ilia <petyavacya@gmail.com>
2018-01-24 15:21:48 +0300

    Update plaintain.sh
    Removed redundant python interpreter call

--- plaintain.sh
+++ plaintain.sh
@@ -3,5 +3,5 @@ set -e
 nfc-mfclassic r a ./dumps/dumpIn.mfd ./dumps/dump0.mfd f
 printf "Dump successfully downloaded. Enter amount: "
 read amount
-python3 ./dumpEdit.py -a ${amount}
+./dumpEdit.py -a ${amount}
 nfc-mfclassic w b ./dumps/dumpOut.mfd ./dumps/dumpIn.mfd f
